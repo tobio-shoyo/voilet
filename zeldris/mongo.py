d057cd515de239a163cbb549b9dd84afa1e07ff3
Anshul Garg <garganshul553@gmail.com>
2021-12-24 21:59:39 +0530

    Update mongo.py

--- zeldris/mongo.py
+++ zeldris/mongo.py
@@ -8,14 +8,14 @@ from pymongo.errors import ServerSelectionTimeoutError
 from zeldris.conf import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
+
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "Violet"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+client = MongoClient(MONGO_DB_URI)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = motor[MONGO_DB]
 db = client["Violet"]
 try:

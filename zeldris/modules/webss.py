a9ddfdbb501c18ddbd6397d5e1ddd3a6aeda0df1
Anshul Garg <garganshul553@gmail.com>
2021-12-25 20:57:45 +0530

    Update webss.py

--- zeldris/modules/webss.py
+++ zeldris/modules/webss.py
@@ -1,30 +1,30 @@
 from pyrogram import filters
-import requests as r
+
 from zeldris import pbot as app
+from zeldris.utlis.errors import capture_err
 
-mod_name = "WebSS"
-help = "/webss [URL] - Take A Screenshot Of A Webpage"
-
+__mod_name__ = "WebSS"
+__help__ = "`/webss` [URL] - Take A Screenshot Of A Webpage"
 
 
 @app.on_message(filters.command("webss"))
+@capture_err
 async def take_ss(_, message):
     try:
         if len(message.command) != 2:
             await message.reply_text("Give A Url To Fetch Screenshot.")
             return
         url = message.text.split(None, 1)[1]
-        m = await message.reply_text("Taking Screenshot")
-        await m.edit("Uploading")
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
         try:
-            x = r.get(f"https://webshot.amanoteam.com/print?q={url}")
             await app.send_photo(
-                       message.chat.id,
-                       photo=x)
+                message.chat.id,
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+            )
         except TypeError:
             await m.edit("No Such Website.")
             return
-            await m.delete()
-        
+        await m.delete()
     except Exception as e:
         await message.reply_text(str(e))

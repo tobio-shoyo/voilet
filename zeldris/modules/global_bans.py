6a474cb0c896711fa3a349fdbbf88dfac354bc8a
Anshul Garg <garganshul553@gmail.com>
2021-12-26 21:24:46 +0530

    Update global_bans.py

--- zeldris/modules/global_bans.py
+++ zeldris/modules/global_bans.py
@@ -431,7 +431,7 @@ def __user_info__(user_id):
         user = sql.get_gbanned_user(user_id)
         if user.reason:
             text += "\nReason: {}".format(html.escape(user.reason))
-            text += "\n\nAppeal at @Mano_sanjiro_support if you think it's invalid."
+            text += "\n\nAppeal at @villainevil_support if you think it's invalid."
     else:
         text = text.format("No")
     return text

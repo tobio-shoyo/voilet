bf0ebce70d9ccc6a0d827d295fdf41c4399eeee0
Anshul Garg <garganshul553@gmail.com>
2021-12-26 12:09:15 +0530

    Update wall.py

--- zeldris/modules/wall.py
+++ zeldris/modules/wall.py
@@ -14,10 +14,10 @@ arq = ARQ(api_url, api_key, session)
 @bot.on_message(filters.command('wall'))
 async def main(_,message):
         wall_ = message.text.replace(message.text.split(' ')[0], '')
-    results = await arq.wall(wall_)
-    wallpaper = results.result
-    x = random.choice(wallpaper)
-    print(x.url_image)
-    await message.reply_photo(photo=x.url_image, caption="Preview")
-    await message.reply_document(x.url_image)
-    await session.close()
+        results = await arq.wall(wall_)
+        wallpaper = results.result
+        x = random.choice(wallpaper)
+        print(x.url_image)
+        await message.reply_photo(photo=x.url_image, caption="Preview")
+        await message.reply_document(x.url_image)
+        await session.close()

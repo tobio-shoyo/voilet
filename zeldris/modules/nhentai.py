0245e2e5bcf312a5b1c998155c66f0a57ff35722
Anshul Garg <garganshul553@gmail.com>
2021-12-27 23:32:38 +0530

    Update nhentai.py

--- zeldris/modules/nhentai.py
+++ zeldris/modules/nhentai.py
@@ -85,9 +85,8 @@ def sauce(update: Update, context: CallbackContext) -> None:
                 InlineKeyboardButton(text="Go to Private Chat", url=context.bot.link)
             ),
         )
-
        elif exception == "group":
-         update.message.reply_html(text=text_blob, reply_markup=markup)
+       update.message.reply_html(text=text_blob, reply_markup=markup)
 
 __help__ = """
 - /sauce `<digits list>` : Read a doujin from nhentai.net in telegram instant preview by giving it's code. 

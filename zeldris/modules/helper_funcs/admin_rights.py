1307b9324526869a2e59669eafc943d74a454da3
Anshul Garg <garganshul553@gmail.com>
2021-12-28 22:08:31 +0530

    Update admin_rights.py

--- zeldris/modules/helper_funcs/admin_rights.py
+++ zeldris/modules/helper_funcs/admin_rights.py
@@ -16,19 +16,22 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from telegram import User, Chat
-
+from zeldris import DEV_USERS, SUPPORT_USERS, DEMONS, WHITELIST_USERS
 
 def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_promote_members
-
+    if user.id in DEV_USERS or in SUPPORT_USERS:
+    return True
 
 def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_restrict_members
-
+    if user.id in DEV_USERS or in SUPPORT_USERS:
+    return True
 
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_pin_messages
-
+    if user.id in DEV_USERS or in SUPPORT_USERS:
+    return True
 
 def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_change_info
@@ -36,3 +39,5 @@ def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
 
 def user_can_delete(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_delete_messages
+    if user.id in DEV_USERS or in SUPPORT_USERS:
+    return True

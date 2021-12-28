1fc70cbd4c1daad36bd6f8f4d8c4befb7eccb564
Anshul Garg <garganshul553@gmail.com>
2021-12-29 01:36:34 +0530

    Update chat_status.py

--- zeldris/modules/helper_funcs/chat_status.py
+++ zeldris/modules/helper_funcs/chat_status.py
@@ -57,6 +57,22 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
         member = chat.get_member(user_id)
     return member.status in ("administrator", "creator")
 
+def is_user_warn_protected(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
+    if (
+        chat.type == "private"
+        or user_id in DEV_USERS
+        or user_id in WHITELIST_USERS
+        or user_id in SUPPORT_USERS
+        or user_id in DEMONS 
+        or chat.all_members_are_administrators
+        or user_id in {777000, 1087968824}
+    ):
+        return True
+
+    if not member:
+        member = chat.get_member(user_id)
+    return member.status in ("administrator", "creator")
+
 
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (

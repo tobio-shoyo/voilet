e916ad7758546b328c713b090f507dbb3963c692
Anshul Garg <garganshul553@gmail.com>
2021-12-28 22:13:56 +0530

    Update admin_rights.py

--- zeldris/modules/helper_funcs/admin_rights.py
+++ zeldris/modules/helper_funcs/admin_rights.py
@@ -20,18 +20,27 @@ from zeldris import DEV_USERS, SUPPORT_USERS, DEMONS, WHITELIST_USERS
 
 def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_promote_members
-    if user.id in DEV_USERS or in SUPPORT_USERS:
-    return True
+    if (
+        user.id in DEV_USERS
+        or user.id in SUPPORT_USERS
+    ):
+        return True
 
 def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_restrict_members
-    if user.id in DEV_USERS or in SUPPORT_USERS:
-    return True
+    if (
+        user.id in DEV_USERS
+        or user.id in SUPPORT_USERS
+    ):
+        return True
 
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_pin_messages
-    if user.id in DEV_USERS or in SUPPORT_USERS:
-    return True
+    if (
+        user.id in DEV_USERS
+        or user.id in SUPPORT_USERS
+    ):
+        return True
 
 def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_change_info
@@ -39,5 +48,8 @@ def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
 
 def user_can_delete(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_delete_messages
-    if user.id in DEV_USERS or in SUPPORT_USERS:
-    return True
+    if (
+        user.id in DEV_USERS
+        or user.id in SUPPORT_USERS
+    ):
+        return True

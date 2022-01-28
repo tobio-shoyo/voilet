2ffffb448f11e7ad2c0ec636408d43e62ed8d677
Anshul Garg <garganshul553@gmail.com>
2022-01-28 10:35:24 +0530

    Update mongo_stats.py

--- zeldris/modules/mongo_stats.py
+++ zeldris/modules/mongo_stats.py
@@ -3,7 +3,7 @@ from zeldris.__main__ import STATS
 from zeldris import DEV_USERS, pbot as app
 from pyrogram import filters
 
-@app.on_message(filters.command("stats") & filters.user(DEV_USERS))
+@app.on_message(filters.command("stamts") & filters.user(DEV_USERS))
 async def stats_cmd(_, message):
     stats_text = "Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS])
     served_users = str(len(await get_served_users()))
